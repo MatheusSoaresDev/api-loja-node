afdbf3e75709ba4d72d33113aa57780761274f25
Matheus Soares <matheus.soares@rarolabs.com.br>
2022-12-26 22:00:15 -0300

    Cadastro de cliente funcional

--- api/controllers/ClienteController.js
+++ api/controllers/ClienteController.js
@@ -2,14 +2,12 @@ const model = require("../models");
 class ClienteController
 {
     static async createCliente(req, res) {
-        console.log(req.body);
-
-        /*try {
+        try {
             const cliente = await model.Cliente.create(req.body);
             return res.status(200).json(cliente);
         } catch (error){
             return res.status(500).json(error.message)
-        }*/
+        }
     }
 }
 
