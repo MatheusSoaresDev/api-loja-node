9d7f4dbb06c7505eda3da75ac26945a63a7145dc
Matheus Soares <matheus.soares@rarolabs.com.br>
2023-01-08 19:40:34 -0300

    chalk instalado.

--- api/controllers/ClienteController.js
+++ api/controllers/ClienteController.js
@@ -1,9 +1,13 @@
 const model = require("../models");
+const chalk = require('chalk')
+
+
 class ClienteController
 {
     static async createCliente(req, res) {
         try {
             const cliente = await model.Cliente.create(req.body);
+            console.log(chalk.blue("cliente cadastrado com sucesso!"))
             return res.status(200).json(cliente);
         } catch (error){
             return res.status(500).json(error.message)
